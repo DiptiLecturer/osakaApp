7f5d90526307a548a626171203f903aa08c81540
DiptiLecturer <lecturer3.amad.nsda@dipti.com.bd>
2026-07-26 08:54:29 +0600

    gallery api added

--- app/src/main/java/org/freedu/osakatelevison/ui/presentation/AppNavigation.kt
+++ app/src/main/java/org/freedu/osakatelevison/ui/presentation/AppNavigation.kt
@@ -60,10 +60,6 @@ fun MainAppScreen() {
             when (currentScreen) {
                 BottomNavItem.Home -> HomeScreen()
                 BottomNavItem.Product -> OsakaProductScreen()
-                BottomNavItem.Gallery -> Text("Photo/Video Gallery")
-                BottomNavItem.Contact -> Text("Contact Form & Details")
-                BottomNavItem.About -> Text("About Osaka TV Details")
-                BottomNavItem.Product -> Text("Products Grid View")
                 BottomNavItem.Gallery -> GalleryScreen()
                 BottomNavItem.Contact -> ContactScreen()
                 BottomNavItem.About ->AboutUsScreen()
